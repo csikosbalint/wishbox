52d9ad38cd18c1aa6d781b14b4feaa1cfcb41c83
csikosbalint <csikos.balint@fnf.hu>
2015-01-11 20:00:36 +0100

    working service export and REST

--- frontend/service/rest-service/src/main/java/hu/fnf/devel/wishbox/frontend/rest/api/RestService.java
+++ frontend/service/rest-service/src/main/java/hu/fnf/devel/wishbox/frontend/rest/api/RestService.java
@@ -1,5 +1,5 @@
 /*
- * RestService.java which is part of the " wishbox ( rest )" project
+ * RestService.java which is part of the " wishbox ( rest-service )" project
  * Copyright (C)  2015  author:  Balint Csikos (csikos.balint@fnf.hu)
  *
  * This program is free software; you can redistribute it and/or
