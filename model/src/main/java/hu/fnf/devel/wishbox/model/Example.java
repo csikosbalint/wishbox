2035c4381ccc6c1bc52d814792f0fcd7e5d62a11
Balint Csikos <csikos.balint@fnf.hu>
2015-12-10 12:31:11 +0100

    exmple test for CI

---
+++ model/src/main/java/hu/fnf/devel/wishbox/model/Example.java
@@ -0,0 +1,10 @@
+package hu.fnf.devel.wishbox.model;
+
+/**
+ * Created by johnnym on 10/12/15.
+ */
+public class Example {
+    public void foo() {
+        System.out.printf("foo");
+    }
+}
---
+++ model/src/test/java/ExampleTest.java
@@ -0,0 +1,14 @@
+import hu.fnf.devel.wishbox.model.Example;
+import org.junit.Test;
+
+/**
+ * Created by johnnym on 10/12/15.
+ */
+public class ExampleTest {
+
+    @Test
+    public void testFoo() throws Exception {
+        Example e = new Example();
+        e.foo();
+    }
+}
